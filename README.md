ffe3a57ce44e621e6708a6218feab64642f7900c
Dennis Hernández <dennishernandezvargas@gmail.com>
2014-12-23 15:18:29 -0600

    First release

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-autoTab
-=======
+# autoTab [![Build Status](https://travis-ci.org/djhvscf/jFont.svg?branch=master)](https://travis-ci.org/djhvscf/autoTab)
+A jQuery plugin that allows the auto tab between inputs and text areas. It is beautiful and lightweight plugin.
 
-A jQuery plugin that allows the auto tab in some input elements
+Author Homepage:      http://djhvscf.github.io/Blog/<br />
+
+## Dependencies
+This plugin has these dependencies
+
+* >=jquery-1.8.x.js
+
+## How to Use
+Define the property `data-tab` in the input element setting a number between 0 to N like `data-tab="0"` and declare `data-length` in the input element setting a number between 1 to N like `data-length="1"`
+this property will be the `maxlength` property in each input element and it helps to plugin knows when change the focus.
+
+**Syntax Example**  
+```html
+<script src="jQuery.min.js"></script>  
+<script src="autoTab.min.js"></script>
+
+<input name="first" id="first" type="text" data-tab="0" data-length="2">
+<input name="second" id="second" type="text" data-tab="1" data-length="2">
+```
+```javascript
+$(function() {
+  // Calling the plugin
+  $.autoTab();
+});
+```
+
+**Parameters**   
+This plugin is not receiving parameters.
+
+**Demo:** [http://djhvscf.github.io/Blog/experiments/2014/12/jFont.html](http://djhvscf.github.io/Blog/experiments/2014/12/jFont.html)
+
+## Reporting issues
+Your feedback is very appreciated! <br />
+Use this page to report issues (https://github.com/djhvscf/autoTab/issues)
--- src/autoTab.js
+++ src/autoTab.js
@@ -7,7 +7,7 @@
  * Created by Dennis Hernández on 23/Dic/2014.
  *
  * Copyright (c) 2014 Dennis Hernández http://djhvscf.github.io/Blog
- *
+ *	
  * The MIT License (http://www.opensource.org/licenses/mit-license.php)
  *
  * Permission is hereby granted, free of charge, to any person
@@ -32,12 +32,70 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-;(function($) {
-    $.fn.autoTab = function(minimum, maximum, middle) {
-        var oSelf = this;
-				
+;(function($) {	
+	$.autoTab = function() {
+		var elements = $('[data-tab]').not('body'),
+		elementHelper = null,
+        notAllowKeys = [9, 16, 37, 38, 39, 40],
+		imposeMaxLength = function (textArea) {
+			if (textArea.value.length <=  parseInt($(textArea).attr('data-length'))) {
+				textArea.value = textArea.value.substr(0, parseInt($(textArea).attr('data-length')));
+				return true;
+			}
+			else {
+				return false;
+			}
+		},
+		selectRange = function (el) {
+			var nextElement = $('[data-tab="' + el + '"]'),
+				start = 0,
+				end;
+			if (!nextElement) {
+				return false;
+			}			
+			if(nextElement.is('input, textarea')) {
+				end = nextElement.get(0).value.length;
+				if (nextElement.setSelectionRange) {
+					nextElement.setSelectionRange(start, end);
+				} else if (nextElement.createTextRange) {
+					var range = nextElement.createTextRange();
+					range.moveStart("character", start);
+					range.moveEnd("character", end - nextElement.value.length);
+					range.select();
+				}
+				nextElement.focus();
+			}
+		},
+		eventKeyUp = function (e) {
+			if($(this).is('body')) {
+				return;
+			}
+			if (!e) {
+				e = window.event;
+			}
+			var oSelf = this;
+			if (!$.inArray(e.keyCode, notAllowKeys)) {
+				if (oSelf.value.length < oSelf.maxLength) {
+					return false;		
+				}
+			} else {
+				if (oSelf.value.length < parseInt($(oSelf).attr('data-length'))) {
+					return false;
+				}
+				else if (oSelf.value.length === parseInt($(oSelf).attr('data-length'))) {
+					selectRange(parseInt($(oSelf).attr('data-tab')) + 1);
+				}
+			}
+		};
+		
+		for(var i = 0; i < elements.length; i++) {
+			elementHelper = $(elements[i]);
+			elementHelper.keyup(eventKeyUp);
+			if(elementHelper.is('input')) {
+				elementHelper.attr('maxlength', elementHelper.attr('data-length'));
+				elementHelper.attr('autocomplete','off');
+			}
+		}
+		elements[0].focus();
     };
-	$(function () {
-        $('[data-tab="0"]').autoTab();
-    });
 })(jQuery);
